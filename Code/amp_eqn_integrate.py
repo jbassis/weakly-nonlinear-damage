d440bffbd22b7acb548778838717156e9b910475
Jeremy Bassis <jbassis@umich.edu>
2025-06-30 15:12:43 -0400

    Added comments to integrate function

--- Code/amp_eqn_integrate.py
+++ Code/amp_eqn_integrate.py
@@ -8,12 +8,24 @@ sys.path.append("../Code/")
 import amp_equation_tools;reload(amp_equation_tools);import amp_equation_tools as amp
 import stress_stream;reload(stress_stream);import stress_stream as stream
 
-def rhs(dt,k,A,S0,bottom_melt,thick,strain_rate):
+
+def integrate(dt,k,A,S0,bottom_melt,thick,strain_rate):
+    """
+    Inputs
+    dt -- time step
+    k -- wavenumber (in units of ice thickness!)
+    A -- amplitude of fundamental mode (in units of ice thickness!)
+    S0 -- dimensionless parameter
+    bottom_melt --melt rate (units m/a or units of ice thickness per time step units)
+    thick -- ice thickness (units m)
+    strain_rate--strain rate (units 1/dt)
+    """
     Scrit,funcs,amp_eqn=amp.solveAmpEqn(k,ms=0.0,mb=bottom_melt/(thick*strain_rate),n=3)
     dAdt = amp_eqn(S=S0,A=A,order=5)
     A += dAdt*dt*strain_rate
     damage =  np.array(stream.amp2dam(k,[A],funcs)).item()
-    return A,damage 
+    k = k*np.exp(-dt*(strain_rate))
+    return A,damage,k 
 
     
     
